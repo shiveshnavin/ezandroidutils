2df648bdaa77ebaaf9612ed51a574630b640debb
Shivesh Navin <shiveshnavin@gmail.com>
2022-04-23 20:09:53 +0530

    Library Done

--- app/src/main/java/com/semibit/sampleapp/SampleActivity.java
+++ app/src/main/java/com/semibit/sampleapp/SampleActivity.java
@@ -1,33 +1,13 @@
 package com.semibit.sampleapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.FirebaseApp;
 import com.semibit.ezandroidutils.EzUtils;
-import com.semibit.ezandroidutils.ui.BaseActivity;
 
-public class SampleActivity extends BaseActivity {
-
-    @Override
-    public void startLogout() {
-
-    }
-
-    @Override
-    public void startHome(Intent intent) {
-
-    }
-
-    @Override
-    public void checkUpdate() {
-
-    }
-
+public class SampleActivity extends SampleBaseActivity {
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
---
+++ app/src/main/java/com/semibit/sampleapp/SampleBaseActivity.java
@@ -0,0 +1,22 @@
+package com.semibit.sampleapp;
+
+import android.content.Intent;
+
+import com.semibit.ezandroidutils.ui.BaseActivity;
+
+public class SampleBaseActivity extends BaseActivity {
+    @Override
+    public void startLogout() {
+
+    }
+
+    @Override
+    public void startHome(Intent intent) {
+
+    }
+
+    @Override
+    public void checkUpdate() {
+
+    }
+}
