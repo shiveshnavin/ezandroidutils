aa329f02b36e1f4acbd5570c5905fbc9ae9b0e30
Shivesh Navin <shiveshnavin@gmail.com>
2022-04-23 20:07:36 +0530

    library working now

--- app/src/main/java/com/semibit/sampleapp/SampleActivity.java
+++ app/src/main/java/com/semibit/sampleapp/SampleActivity.java
@@ -1,4 +1,39 @@
 package com.semibit.sampleapp;
 
-public class SampleActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.FirebaseApp;
+import com.semibit.ezandroidutils.EzUtils;
+import com.semibit.ezandroidutils.ui.BaseActivity;
+
+public class SampleActivity extends BaseActivity {
+
+    @Override
+    public void startLogout() {
+
+    }
+
+    @Override
+    public void startHome(Intent intent) {
+
+    }
+
+    @Override
+    public void checkUpdate() {
+
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FirebaseApp.getInstance();
+        EzUtils.toast(this,"OK");
+
+    }
 }
---
+++ app/src/main/java/com/semibit/sampleapp/SampleApp.java
@@ -0,0 +1,18 @@
+package com.semibit.sampleapp;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.FirebaseApp;
+import com.semibit.ezandroidutils.App;
+
+public class SampleApp extends App {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
